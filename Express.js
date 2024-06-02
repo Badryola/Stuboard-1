aa87427302e60d338e865dd1fd8746c2f53ad1a4
unknown <bedoelbadryua@gmail.com>
2024-06-03 00:34:23 +0300

    first commit

--- Express.js
+++ Express.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: '',
     database: 'students_db'
 });
 
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Stuboard
+# MyProject
